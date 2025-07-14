388d89d54d4e95b2eddf54b592a2bb1a3e23237b
PrincessDaniel <princessdaniel928@gmail.com>
2025-07-14 05:48:58 +0100

    update image names in data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `./troll.jpg`,
+        profilePic: `troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,14 +13,14 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `./musk.png`,
+        profilePic: `musk.png`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
-                  {
+            {
                 handle: `@TomCruise ✅`,
-                profilePic: `./tcruise.png`,
+                profilePic: `tcruise.png`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
                 replies: [],
                 likes: 0,
@@ -29,9 +29,9 @@ export const tweetsData = [
                 isRetweeted: false,
                 uuid: '3dnm454ee-c0f5-9g12-9c4b-77835tgs2'
             },
-                  {
+            {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `./chucknorris.jpeg`,
+                profilePic: `chucknorris.jpeg`,
                 tweetText: `I went last year😴`,
                 replies: [],
                 likes: 0,
@@ -45,17 +45,17 @@ export const tweetsData = [
         isRetweeted: false,
         uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
     },
-        {
+    {
         handle: `@NoobCoder12`,
-        profilePic: `./flower.png`,
+        profilePic: `flower.png`,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
         replies: [
             {
                 handle: `@StackOverflower ☣️`,
-                profilePic: `./overflow.png`,
-                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+                profilePic: `overflow.png`,
+                tweetText: `No. Obviously not. Go get a job in McDonald's.`,
                 replies: [],
                 likes: 0,
                 retweets: 0,
@@ -65,7 +65,7 @@ export const tweetsData = [
             },
             {
                 handle: `@YummyCoder64`,
-                profilePic: `./love.png`,
+                profilePic: `love.png`,
                 tweetText: `You are wonderful just as you are! ❤️`,
                 replies: [],
                 likes: 0,
